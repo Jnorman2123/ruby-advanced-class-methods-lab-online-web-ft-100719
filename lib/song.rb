e06f316f9b98a797ed102aa9393c235a80e9ce32
Jonathan Norman <jnorman2123@gmail.com>
2019-10-15 20:35:30 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -35,8 +35,7 @@ class Song
 
   def self.find_or_create_by_name(song)
     if self.find_by_name(song)
-      song.name = song
-      song
+      @name
     else
        self.create_by_name(song)
     end
