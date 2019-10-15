359e82043fcd593e32c5695c15bd3a70e75a1e23
Jonathan Norman <jnorman2123@gmail.com>
2019-10-15 23:35:00 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -45,6 +45,7 @@ class Song
     new_song = self.new
     new_song_object = song.split(" - ")
     new_song.name = new_song_object[1].delete_suffix(".mp3")
+    new_song.artist_name = new_song_object[0]
     new_song
   end
 end
